1040b902b4757fcb7b5670a71a6261b82b298504
Elizabeth Díaz Orea <odelys2003@hotmail.com>
2013-05-17 21:22:33 -0500

    dsd

--- js/API/captura.js
+++ js/API/captura.js
@@ -2,8 +2,8 @@
 function tomarFoto(){
 	navigator.device.capture.captureImage(function(mediaFiles){
 		path = mediaFiles[0].fullPath;
-		$('#regFoto').append('<img src="'+path+'" width="100%" />').attr('rel',path);
+		$('#regFoto').append('<br><img src="'+path+'" width="100%" />').attr('rel',path);
 	},function(error){
 		navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-	},{limit:2});
+	},{limit:1});
 }
